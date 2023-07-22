a0d9c9f8f32d8fde8d0925c9367bf5ec7a213399
CarlosMario <105567234+CarlosMario123@users.noreply.github.com>
2023-07-22 11:10:49 -0600

    Add files via upload

--- routes/routesProducto.js
+++ routes/routesProducto.js
@@ -1,120 +1,22 @@
 const express = require("express");
+const { obtenerProductos, obtenerProducto, productoID, agregarProducto, eliminarProducto, actualizarProducto, productoNombre } = require("../controllers/productoController");
 const routesProducto = express.Router();
 
-routesProducto.get("/", (req, res) => {
-  req.getConnection((err, conn) => {
-    if (err) return res.send(err);
+routesProducto.get("/", obtenerProductos);
 
-    conn.query("SELECT * FROM Producto", (err, rows) => {
-      if (err) return res.send(err);
-
-      res.json(rows);
-    });
-  });
-});
-
-routesProducto.get("/:nombre", (req, res) => {
-  req.getConnection((err, conn) => {
-    if (err) return res.send(err);
-    conn.query(
-      "SELECT Nombre,Cantidad_Disponible FROM Producto WHERE Nombre = ?",
-      [req.params.nombre],
-      (err, rows) => {
-        if (err) return res.send(err);
-        res.json(rows);
-      }
-    );
-  });
-});
+routesProducto.get("/name/:id", obtenerProducto);
 
 
-routesProducto.get("/id/:id", (req, res) => {//extraer producto por id
-    console.log(req.params.id)
-  req.getConnection((err, conn) => {
-    if (err) return res.send(err);
-    conn.query(
-      "SELECT Cantidad_Disponible FROM Producto WHERE id_Producto = ?",
-      [req.params.id],
-      (err, rows) => {
-        if (err) return res.send(err);
-        res.json(rows);
-      }
-    );
-  });
-});
-
-routesProducto.post("/", (req, res) => {
-  req.getConnection((err, conn) => {
-    if (err) return res.send(err);
-
-    const productoData = req.body;
-    const idProducto = productoData.id_Producto;
-    const cantidadIntroducida = productoData.Cantidad_Disponible;
-
-    conn.query(
-      "SELECT id_Producto, Cantidad_Disponible FROM Producto WHERE id_Producto = ?",
-      [idProducto],
-      (err, result) => {
-        if (err) return res.send(err);
-
-        if (result.length === 0) {
-          // El producto no existe en la tabla Producto, se realiza la inserción
-          conn.query(
-            "INSERT INTO Producto SET ?",
-            [productoData],
-            (err, result) => {
-              if (err) return res.send(err);
-
-              res.json(result);
-            }
-          );
-        } else {
-          // El producto existe, se realiza la actualización de la cantidad disponible
-          const cantidadActual = result[0].Cantidad_Disponible;
-          const nuevaCantidad = cantidadActual + cantidadIntroducida;
-
-          conn.query(
-            "UPDATE Producto SET Cantidad_Disponible = ? WHERE id_Producto = ?",
-            [nuevaCantidad, idProducto],
-            (err, result) => {
-              if (err) return res.send(err);
-
-              res.json(result);
-            }
-          );
-        }
-      }
-    );
-  });
-});
+routesProducto.get("/:nombre", productoNombre);
 
 
-routesProducto.delete("/:id", (req, res) => {
-  req.getConnection((err, conn) => {
-    if (err) return res.send(err);
-    conn.query(
-      "DELETE FROM Producto WHERE id_Producto = ?",
-      [req.params.id],
-      (err, rows) => {
-        if (err) return res.send(err);
+routesProducto.get("/id/:id", productoID);
 
-        res.json(rows);
-      }
-    );
-  });
-});
+routesProducto.post("/", agregarProducto);
 
-routesProducto.put("/:id", (req, res) => {
-  req.getConnection((err, conn) => {
-    if (err) return res.send(err);
-    conn.query(
-      "UPDATE Producto set ? WHERE id_Producto = ?",
-      [req.body, req.params.id],
-      (err, rows) => {
-        if (err) return res.send(err);
-      }
-    );
-  });
-});
+
+routesProducto.delete("/:id", eliminarProducto);
+
+routesProducto.put("/:id", actualizarProducto);
 
 module.exports = routesProducto;
